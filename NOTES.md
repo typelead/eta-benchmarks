b3ea874706e7078c05df44b926757a4b8a298024
Rahul Muttineni <rahulmutt@gmail.com>
2017-04-30 15:02:03 +0530

    Disabled spectral/clausify and spectral/constraints

--- NOTES.md
+++ NOTES.md
@@ -6,5 +6,9 @@ The following were disabled because of exceeding the 4G memory limit on CircleCI
 
 - `imaginary/digits-of-e1`
 - `imaginary/digits-of-e2`
+- `spectral/clausify`
+- `spectral/constraints`
 - `shootout/pidigits`
+
+The following were disabled because of taking too much time:
 - `spectral/circsim`
--- slow-benchmarks.sh
+++ slow-benchmarks.sh
@@ -3,4 +3,4 @@
 etlas install transformers array containers bytestring
 stack install
 eta-bench clean
-eta-bench imaginary/digits-of-e1 imaginary/digits-of-e2 shootout/pidigits spectral/circsim --way="-O2" --jmh="-wi 5 -i 5" --run
+eta-bench imaginary/digits-of-e1 imaginary/digits-of-e2 shootout/pidigits spectral/circsim spectral/clausify spectral/constraints --way="-O2" --jmh="-wi 5 -i 5" --run
