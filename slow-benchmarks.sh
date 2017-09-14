134fcc99b4a8826ff19cc9a5be9fd456122fc4eb
Rahul Muttineni <rahulmutt@gmail.com>
2017-09-14 04:26:04 -0400

    Make slow benchmarks 0 1

--- slow-benchmarks.sh
+++ slow-benchmarks.sh
@@ -3,4 +3,4 @@
 etlas install transformers array containers bytestring
 stack install
 eta-bench clean
-eta-bench imaginary/digits-of-e1 imaginary/digits-of-e2 shootout/pidigits spectral/circsim spectral/clausify spectral/constraints shootout/n-body --way="-O2" --jmh="-wi 10 -i 10" --run
+eta-bench imaginary/digits-of-e1 imaginary/digits-of-e2 shootout/pidigits spectral/circsim spectral/clausify spectral/constraints shootout/n-body --way="-O2" --jmh="-wi 0 -i 1" --run
