fe3dd7f5093301bcf9688aacd8bbdef1b4d29f61
Rahul Muttineni <rahulmutt@gmail.com>
2019-01-10 15:58:03 +0530

    Change quick to fast

--- quick-benchmarks.sh
+++ quick-benchmarks.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-BENCHMARKS=$(cat benchmarks | awk '{if ($1 == "quick") print $2}')
+BENCHMARKS=$(cat benchmarks | awk '{if ($1 == "fast") print $2}')
 
 etlas run eta-bench -- $BENCHMARKS --way="-O2" --jmh="-wi 0 -i 1 -gc true -prof gc" --run
