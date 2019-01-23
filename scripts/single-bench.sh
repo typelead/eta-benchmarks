be0d193be8a2729da0943b28dff3ce273e2a66c5
Roland Tritsch <roland@tritsch.org>
2019-01-23 08:02:07 +0000

    Convert comment into var names (to improve readability/maintainability).

--- scripts/single-bench.sh
+++ scripts/single-bench.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
-# ARGS: [test-name] [measurement iterations] [worker iterations]
+TEST_NAME=${1}
+MEASUREMENT_ITERATIONS=${2}
+WORKER_ITERATIONS=${3}
 
-# eta-bench "$1" --way="-O2" --jmh="-wi $3 -i $2 -bm ss -gc true -prof gc" --run
-etlas run eta-bench -- "$1" --way="-O2" --jmh="-wi $3 -i $2 -bm ss -gc true -prof gc" --run --compiler="./scripts/eta.sh"
+etlas run eta-bench -- "${TEST_NAME}" --way="-O2" --jmh="-wi ${WORKER_ITERATIONS} -i ${MEASUREMENT_ITERATIONS} -bm ss -gc true -prof gc" --run --compiler="./scripts/eta.sh"
