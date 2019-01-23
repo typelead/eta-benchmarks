5516f12b40a67da43a01e3b6f540d798bd8811f9
Roland Tritsch <roland@tritsch.org>
2019-01-23 09:42:00 +0000

    Make fast and single run with the same flags/configuration (so that I can test better).

--- scripts/fast-benchmarks.sh
+++ scripts/fast-benchmarks.sh
@@ -4,5 +4,5 @@ BENCHMARKS=$(cat ./scripts/benchmarks | awk '{if ($1 == "fast") print $2}')
 
 for b in ${BENCHMARKS}
 do
-  etlas run eta-bench -- ${b} --way="-O2" --jmh="-wi 1 -i 5 -gc true -prof gc -f 3" --run --compiler="./scripts/eta.sh" || exit
+  etlas run eta-bench -- ${b} --way="-O2" --jmh="-wi 1 -i 5 -gc true -prof gc -f 1 -bm all" --run --compiler="./scripts/eta.sh" || exit
 done
--- scripts/single-bench.sh
+++ scripts/single-bench.sh
@@ -4,4 +4,4 @@ TEST_NAME=${1}
 MEASUREMENT_ITERATIONS=${2}
 WORKER_ITERATIONS=${3}
 
-etlas run eta-bench -- "${TEST_NAME}" --way="-O2" --jmh="-wi ${WORKER_ITERATIONS} -i ${MEASUREMENT_ITERATIONS} -bm ss -gc true -prof gc" --run --compiler="./scripts/eta.sh"
+etlas run eta-bench -- "${TEST_NAME}" --way="-O2" --jmh="-wi ${WORKER_ITERATIONS} -i ${MEASUREMENT_ITERATIONS} -gc true -prof gc -f 1 -bm all" --run --compiler="./scripts/eta.sh"
