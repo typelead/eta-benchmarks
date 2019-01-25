ae845173ced8289b98c2dc7f2ed7c0ead08b5ce0
Roland Tritsch <roland@tritsch.org>
2019-01-25 08:14:36 +0000

    Clarify what -wi is.

--- scripts/single-bench.sh
+++ scripts/single-bench.sh
@@ -2,6 +2,6 @@
 
 TEST_NAME=${1}
 MEASUREMENT_ITERATIONS=${2}
-WORKER_ITERATIONS=${3}
+WARMUP_ITERATIONS=${3}
 
-etlas run eta-bench -- "${TEST_NAME}" --way="-O2" --jmh="-wi ${WORKER_ITERATIONS} -i ${MEASUREMENT_ITERATIONS} -gc true -prof gc -f 1 -bm all" --run --compiler="./scripts/eta.sh"
+etlas run eta-bench -- "${TEST_NAME}" --way="-O2" --jmh="-i ${MEASUREMENT_ITERATIONS} -wi ${WARMUP_ITERATIONS} -gc true -prof gc -f 1 -bm all" --run --compiler="./scripts/eta.sh"
